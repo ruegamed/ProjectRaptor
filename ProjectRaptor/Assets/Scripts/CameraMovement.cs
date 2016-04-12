1f77bfe75d6639897e4d08af61f9907dece215fd
ruegamed <ruegamed@email.sc.edu>
2016-04-12 03:01:12 -0400

    Unity Project
    The Unity Project

---
+++ ProjectRaptor/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{	
+	void Update () 
+	{
+		float cameraPanX;
+		float cameraPanZ;
+		float cameraRotate;
+		Vector3 cameraRotation;
+
+		cameraPanX = Input.GetAxis("CameraPanHorizontal");
+		cameraPanZ = Input.GetAxis("CameraPanVertical");
+		cameraRotate = Input.GetAxis("CameraRotate");
+
+		cameraRotation = gameObject.transform.rotation.eulerAngles;
+		cameraRotation.y += cameraRotate;
+
+		gameObject.transform.position += gameObject.transform.forward * cameraPanZ;
+		gameObject.transform.position += gameObject.transform.right * cameraPanX;
+		gameObject.transform.rotation = Quaternion.Euler(cameraRotation);
+	}
+}
---
+++ ProjectRaptor/Assets/Scripts/SpriteFace.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class SpriteFace : MonoBehaviour 
+{
+	void Start()
+	{
+		
+	}
+
+	void Update () 
+	{
+		Vector3 cameraPosition;
+		Quaternion currentRotation;
+		float lockX = 0;
+
+		cameraPosition = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+
+		this.gameObject.transform.LookAt(cameraPosition);
+		currentRotation = this.gameObject.transform.rotation;
+		currentRotation.x = lockX;
+		currentRotation.z = lockX;
+
+		this.gameObject.transform.localRotation = currentRotation;
+	}
+}
