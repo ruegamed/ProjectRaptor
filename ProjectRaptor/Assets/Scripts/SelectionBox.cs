68df525337122887927425874fbe038f902851e9
ruegamed <ruegamed@email.sc.edu>
2016-04-12 22:39:54 -0400

    Added Box Selection
    - A selection box can now be used in play mode
    - The characters turning black indicates selection
    - Click anywhere to deselect

---
+++ ProjectRaptor/Assets/Scripts/SelectionBox.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//THANKS TO Brent Farris on youtube for this
+public class SelectionBox : MonoBehaviour
+{
+	public Texture2D selectionBox = null;
+	public static Rect selection = new Rect(0, 0, 0, 0);
+	private Vector3 startClick = -Vector3.one;
+
+	void Update () 
+	{
+		cameraCheck();
+	}
+
+	private void cameraCheck()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			startClick = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			startClick = -Vector3.one;
+		}
+
+		if(Input.GetMouseButton(0))
+		{
+			selection = new Rect(startClick.x, InvertMouseY(startClick.y), Input.mousePosition.x - startClick.x, 
+				InvertMouseY(Input.mousePosition.y) - InvertMouseY(startClick.y));
+
+			if(selection.width < 0)
+			{
+				selection.x += selection.width;
+				selection.width = -selection.width;
+			}
+			if(selection.height < 0)
+			{
+				selection.y += selection.height;
+				selection.height = -selection.height;
+			}
+		}
+	}
+
+	private void OnGUI()
+	{
+		if(startClick != -Vector3.one)
+		{
+			GUI.color = new Color(1, 1, 1, 0.3F);
+			GUI.DrawTexture(selection, selectionBox);
+		}
+	}
+
+	public static float InvertMouseY(float y)
+	{
+		return Screen.height - y;
+	}
+}
---
+++ ProjectRaptor/Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//THANKS TO Brent Farris on youtube for this
+public class UnitSelection : MonoBehaviour 
+{
+	public bool selected = false;
+
+	void Update () 
+	{
+		if(Input.GetMouseButtonUp(0))
+		{
+			Vector3 cameraPosition = Camera.main.WorldToScreenPoint(transform.position);
+			cameraPosition.y = SelectionBox.InvertMouseY(cameraPosition.y);
+			selected = SelectionBox.selection.Contains(cameraPosition);
+			Debug.Log(selected);
+		}
+
+		if(selected)
+		{
+			SpriteRenderer pic = gameObject.GetComponent<SpriteRenderer>();
+			Color newColor = new Color(0, 0, 0, 1);
+
+			pic.color = newColor;
+		}
+		else
+		{
+			SpriteRenderer pic = gameObject.GetComponent<SpriteRenderer>();
+			Color newColor = new Color(1, 1, 1, 1);
+
+			pic.color = newColor;
+		}
+	}
+}
