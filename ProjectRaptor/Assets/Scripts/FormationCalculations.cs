ea196ff64ea0738e3891d3fb5f87fbcbb72d6cda
ruegamed <ruegamed@email.sc.edu>
2016-04-28 04:44:44 -0400

    Development Update 1.1: Formations!
    - Formations have been added
    - The following can now be used: Short Line, Loose Line, and Long Line
    - Stats for each formation can be modified on the "PlayerController"
    object
    - The "FormationCalculations" script will handle all calculations for
    all formations
    - The "PlayerCommands" script has been updated to account for formations
    - A prefab for the "PlayerController" has been added so that modifying
    it should become easier

---
+++ ProjectRaptor/Assets/Scripts/FormationCalculations.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FormationCalculations : MonoBehaviour
+{
+	public LineData ShortLineData;
+	public LineData LongLineData;
+	public LineData LooseLineData;
+
+	void Start()
+	{
+	
+	}
+
+	public List<Vector3> getFormationPositions(int groupSize, Vector3 groupLocation, Vector3 moveLocation, int formation)
+	{
+		if(formation == 0)
+		{
+			//Debug.Log("Short Line");
+			return getLinePositions(groupSize, groupLocation, moveLocation, ShortLineData);
+		}
+		else if(formation == 1)
+		{
+			//Debug.Log("Long Line");
+			return getLinePositions(groupSize, groupLocation, moveLocation, LongLineData);
+		}
+		else// if(formation == 2)
+		{
+			//Debug.Log("Loose Line");
+			return getLinePositions(groupSize, groupLocation, moveLocation, LooseLineData);
+		}
+	}
+
+	List<Vector3> getLinePositions(int groupSize, Vector3 groupLocation, Vector3 moveLocation, LineData lineData)
+	{
+		List<Vector3> movePositions;
+
+		GameObject moveReferenceObject;
+		int unitsRemaining;
+
+		int unitsThisLine;
+		float lengthOfLine;
+		bool cont;
+
+		Vector3 lastPosition;
+
+		movePositions = new List<Vector3>();
+
+		moveReferenceObject = createMoveReferenceObject(groupLocation, moveLocation);
+		unitsRemaining = groupSize;
+
+		cont = true;
+
+		while(cont)
+		{
+			if(unitsRemaining > lineData.unitsPerLine)
+			{
+				unitsThisLine = lineData.unitsPerLine;
+				unitsRemaining -= lineData.unitsPerLine;
+			}
+			else
+			{
+				unitsThisLine = unitsRemaining;
+				unitsRemaining = 0;
+				cont = false;
+			}
+
+			lengthOfLine = (unitsThisLine - 1) * lineData.horizontalSpacing;
+
+			lastPosition = moveReferenceObject.transform.position - moveReferenceObject.transform.right * (0.5F * lengthOfLine);
+
+			for(int x = 0; x != unitsThisLine; x++)
+			{
+				Vector3 newMovePosition;
+
+				newMovePosition = new Vector3();
+
+				newMovePosition = lastPosition + moveReferenceObject.transform.right * lineData.horizontalSpacing * x;
+
+				movePositions.Add(newMovePosition);
+			}
+
+			moveReferenceObject.transform.position -= moveReferenceObject.transform.forward * lineData.verticalSpacing;
+		}
+
+		Destroy(moveReferenceObject);
+
+		return movePositions;
+	}
+
+	Vector3 lineCalc(int unitIndex, int groupSize)
+	{
+		Vector3 movePosition;
+
+		movePosition = new Vector3();
+
+		return movePosition;
+	}
+
+	GameObject createMoveReferenceObject(Vector3 groupLocation, Vector3 moveLocation)
+	{
+		GameObject moveObject;
+
+		moveObject = new GameObject();
+		moveObject.transform.position = moveLocation;
+		moveObject.transform.LookAt(groupLocation);
+		moveObject.transform.Rotate(0, 180, 0);
+
+		return moveObject;
+	}
+
+	[System.Serializable]
+	public class LineData
+	{
+		public int unitsPerLine;
+		public float horizontalSpacing;
+		public float verticalSpacing;
+	}
+}
--- ProjectRaptor/Assets/Scripts/PlayerCommands.cs
+++ ProjectRaptor/Assets/Scripts/PlayerCommands.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 
 public class PlayerCommands : MonoBehaviour
 {
+	public int currentFormation;
 	public List<GameObject> selectionGroup;
+
 	List<NavMeshAgent> selectionGroupAgents;
+	FormationCalculations formationCalculations;
 
 	void Start()
 	{
+		currentFormation = 0;
 		selectionGroup = new List<GameObject>();
 		selectionGroupAgents = new List<NavMeshAgent>();
+		formationCalculations = gameObject.GetComponent<FormationCalculations>();
 	}
 
 	void Update()
@@ -47,10 +52,39 @@ public class PlayerCommands : MonoBehaviour
 
 		if(Physics.Raycast(mousePosition, out movePoint))
 		{
+			List<Vector3> movePositions;
+
+			movePositions = formationCalculations.getFormationPositions(selectionGroup.Count, calculateGroupPosition(), movePoint.point, currentFormation);
+
 			for(int x = 0; x != selectionGroupAgents.Count; x++)
 			{
-				selectionGroupAgents[x].destination = movePoint.point;
+				//Debug.Log(movePositions[x]);
+				selectionGroupAgents[x].destination = movePositions[x];
 			}
 		}
+	}
+
+	private Vector3 calculateGroupPosition()
+	{
+		Vector3 groupPosition;
+
+		float xAverage;
+		float zAverage;
+
+		xAverage = 0;
+		zAverage = 0;
+
+		for(int x = 0; x != selectionGroup.Count; x++)
+		{
+			xAverage += selectionGroup[x].transform.position.x;
+			zAverage += selectionGroup[x].transform.position.z;
+		}
+
+		xAverage = xAverage / selectionGroup.Count;
+		zAverage = zAverage / selectionGroup.Count;
+
+		groupPosition = new Vector3(xAverage, 0, zAverage);
+
+		return groupPosition;
 	}
 }
